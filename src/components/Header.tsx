cdfec01dd9b8cf4f0d5f5e4ad05ae897e1744393
Chad Chapman <chad.chapman2010@gmail.com>
2023-03-26 18:35:13 -0400

    Added content for header in render function.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,26 @@
 /******************************************************************************
  * IMPORTS                                                                    *
  *****************************************************************************/
-import React from "react";
+import React, { Component } from 'react';
 
-
-class Header {
-
+/**
+ * @class This class is responsble for rendering the header component of the 
+ * webpage.
+ * @author Chad Chapman
+ * @extends Component
+ */
+class Header extends Component{
+    /**
+     * Render function for the header component of this webpage.
+     * @returns HTMLDivElement containing the header.
+     */
+    render() {
+        return(
+            <div className="header">
+                <h1>CV Application</h1>
+            </div>
+        )
+    }
 }
 
 export default Header;
