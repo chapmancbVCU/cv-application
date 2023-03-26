aa78c2d864df0d52f88d912c8277bf02902605c5
Chad Chapman <chad.chapman2010@gmail.com>
2023-03-26 19:18:51 -0400

    Added class to main and flexbox styling.

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -8,7 +8,7 @@ import React, { Component } from 'react';
 class Main extends Component {
     render() {
         return(
-            <div>
+            <div className="main">
                 <Form />
                 <CV />
             </div>
