dd78fddac55d5a9e8633f3b328be60a024600a24
Chad Chapman <chad.chapman2010@gmail.com>
2023-03-26 18:55:06 -0400

    Created for and CV components.  We print a test message.

---
+++ src/components/CV.tsx
@@ -0,0 +1,12 @@
+/******************************************************************************
+ * IMPORTS                                                                    *
+ *****************************************************************************/
+import React, { Component } from 'react';
+
+class CV extends Component {
+    render() {
+        return <p>CV section</p>;
+    }
+}
+
+export default CV;
---
+++ src/components/Form.tsx
@@ -0,0 +1,12 @@
+/******************************************************************************
+ * IMPORTS                                                                    *
+ *****************************************************************************/
+import React, { Component } from 'react';
+
+class Form extends Component {
+    render() {
+        return <p>Form</p>
+    }
+}
+
+export default Form;
