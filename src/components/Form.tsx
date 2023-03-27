384f72d53615177c497b1be60b507c3c74c8dd3b
Chad Chapman <chad.chapman2010@gmail.com>
2023-03-27 19:43:04 -0400

    We can show what is typed on page while typing.

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -8,7 +8,15 @@ import React, { Component } from 'react';
  * @author Chad Chapman
  * @extends Component
  */
-class Form extends Component {
+class Form extends Component<{}, { firstName: string }> {
+    constructor(props: any){
+        super(props);
+
+        this.state = {
+            firstName: ''
+        }
+    }
+
     /**
      * Render function for the form component of this webpage.
      * @returns HTMLDivElement containing the form.
@@ -17,6 +25,25 @@ class Form extends Component {
         return(
             <div className="form-container">
                 <h2>Personal Information</h2>
+                <hr></hr>
+                <form className="cv-form">
+                    <div className="form-row">
+                        <label htmlFor="first-name">First Name</label>
+                        <input 
+                            id="first-name" 
+                            name="first-name" 
+                            type="text"
+                            placeholder="Joe"
+                            onChange={event => this.setState({firstName: event.target.value})}
+                            value={this.state.firstName}/>
+                    </div>
+                    {this.state.firstName}
+                    <div className="form-row">
+                        <label htmlFor="last-name">Last Name</label>
+                        <input id="last-name" name="last-name" type="text" />
+                    </div>
+
+                </form>
             </div>
         );
     }
