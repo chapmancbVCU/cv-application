e5c26eeb68afc19941cd456e55a036f1e5ee1bef
Chad Chapman <chad.chapman2010@gmail.com>
2023-03-29 20:37:21 -0400

    Added function to handle details state.

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -6,9 +6,14 @@ import Form from './Form';
 import React, { useState } from 'react';
 
 function Main() {
+    const [details, setDetails] = useState("");
     const [firstName, setFirstName] = useState("");
     const [lastName, setLastName] = useState("");
     const [title, setTitle] = useState("");
+
+    function handleDetailsInput(event: any) {
+        setDetails(event.target.value);
+    }
 
     function handleFirstNameInput(event: any) {
         setFirstName(event.target.value);
@@ -24,14 +29,17 @@ function Main() {
 
     return(
         <div className="main">
-            <Form firstName={firstName} 
+            <Form details={details}
+                firstName={firstName} 
+                handleDetailsInput={handleDetailsInput}
                 handleFirstNameInput={handleFirstNameInput}
                 handleLastNameInput={handleLastNameInput}
                 handleTitleInput={handleTitleInput}
                 lastName={lastName} 
                 title={title} 
             />
-            <CV firstName={firstName} 
+            <CV details={details}
+                firstName={firstName} 
                 lastName={lastName}
                 title={title}
             />
